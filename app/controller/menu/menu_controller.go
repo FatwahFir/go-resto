1dea455d76752d3c61368759a024018d3b6199f0
Fatwah Firmansyah <fatwah091@gmail.com>
2023-12-06 13:34:35 +0700

    File upload success

--- app/controller/menu/menu_controller.go
+++ app/controller/menu/menu_controller.go
@@ -62,41 +62,53 @@ func Create(c *fiber.Ctx) error {
 }
 
 func Update(c *fiber.Ctx) error {
-
+	// Mengambil data menu dari database berdasarkan id
 	menu := new(models.Menu)
-
-	oldImage := database.DB.Select("image").Take(&models.Menu{}, "id = ?", c.Params("id")).Error
-
-	if err := c.BodyParser(menu); err != nil {
+	if err := database.DB.Take(&menu, "id = ?", c.Params("id")).Error; err != nil {
 		log.Errorf("menu update: %v", err)
 		return err
 	}
 
-	filename := oldImage
-	menu.CategoryID = c.FormValue("category_id")
-	menu.PriceModal, _ = strconv.Atoi(c.FormValue("price_modal"))
-	if err := helpers.Validate.Struct(menu); err != nil {
+	// Membuat objek baru untuk menyimpan data request
+	request := new(models.Menu)
+	// Mengisi objek request dengan data dari body request
+	if err := c.BodyParser(request); err != nil {
 		log.Errorf("menu update: %v", err)
 		return err
 	}
 
-	if file, _ := c.FormFile("image"); file != nil {
-		// fmt.Println(file)
+	// Menyimpan nama file yang saat ini digunakan
+	filename := menu.Image
+
+	// Mengisi field CategoryID dan PriceModal dari form value
+	request.CategoryID = c.FormValue("category_id")
+	request.PriceModal, _ = strconv.Atoi(c.FormValue("price_modal"))
+
+	// Melakukan validasi
+	if err := helpers.Validate.Struct(request); err != nil {
+		log.Errorf("menu update: %v", err)
+		return err
+	}
+
+	// Cek apakah ada file yang diupload
+	if file, err := c.FormFile("image"); err == nil {
+		// Jika ada file yang diupload, simpan dengan nama baru
 		filename = time.Now().Format("20060102150405") + "-" + file.Filename
-		c.SaveFile(file, "./storage/menu-images/"+filename)
+		os.Remove("./storage/menu-images/" + menu.Image)
+		if err := c.SaveFile(file, "./storage/menu-images/"+filename); err != nil {
+			log.Errorf("menu update: %v", err)
+			return err
+		}
 	}
-	menu.Image = filename
-	// if err != nil {
-	// 	log.Errorf("menu create: %v", err)
-	// 	return err
-	// }
 
-	// id := c.Params("id")
-	// err := database.DB.Model(&models.Category{}).Where("id = ?", id).Update("name", menu.Name).Error
-	// if err != nil {
-	// 	log.Errorf("menu update: %v", err)
-	// 	return err
-	// }
+	// Mengganti nama file yang ada dengan nama yang baru (atau tetap sama jika tidak ada file yang diupload)
+	request.Image = filename
+
+	// Mengupdate data menu di database
+	if err := database.DB.Model(&models.Menu{}).Where("id = ?", c.Params("id")).Updates(request).Error; err != nil {
+		log.Errorf("menu update: %v", err)
+		return err
+	}
 
 	return helpers.OkResponse(c, "Update data successfully", nil)
 }
